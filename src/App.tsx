bb6828fa8b884d06118d065e3873e0718859b3ef
Jason Stine <lostlaowai@outlook.com>
2022-03-28 10:52:47 +0800

    feat: basic page routing and app container setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,44 +1,19 @@
-import React, { useState } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { AppContainer } from './App.styles';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Dashboard from './pages/Dashboard';
+import Client from './pages/Client';
 
 const App: React.FC = () => {
-  const [count, setCount] = useState(0);
-
   return (
-    <div className='App'>
-      <header className='App-header'>
-        <img src={logo} className='App-logo' alt='logo' />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type='button' onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.tsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className='App-link'
-            href='https://reactjs.org'
-            target='_blank'
-            rel='noopener noreferrer'
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className='App-link'
-            href='https://vitejs.dev/guide/features.html'
-            target='_blank'
-            rel='noopener noreferrer'
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
-    </div>
+    <AppContainer>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/dashboard' element={<Dashboard />} />
+          <Route path='/client' element={<Client />} />
+        </Routes>
+      </BrowserRouter>
+    </AppContainer>
   );
 };
 
