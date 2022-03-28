d4fb081da16fd3deca13cc38c076a8ef8238fd60
Jason Stine <lostlaowai@outlook.com>
2022-03-28 10:34:38 +0800

    feat: add firebase

---
+++ src/firebaseConfig.ts
@@ -0,0 +1,17 @@
+import { initializeApp } from 'firebase/app';
+import { getFirestore } from 'firebase/firestore';
+
+const firebaseConfig = {
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  databaseURL: import.meta.env.VITE_FIREBASE_DATABASE_URL,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+} as { [key: string]: string | undefined };
+
+const app = initializeApp(firebaseConfig);
+const firestoreDB = getFirestore(app);
+
+export { firestoreDB };
